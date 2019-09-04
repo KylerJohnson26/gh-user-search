8531d58b68a3bb8d402c628933dec6bcdde4938b
KylerJohnson26 <kylerljohnson@icloud.com>
2019-09-04 08:00:24 -0400

    setting up with firebase deploy and removing environment files from public repo

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,12 +2,16 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpClientModule } from '@angular/common/http';
+import { AngularFireModule } from '@angular/fire';
+import { AngularFireAuthModule } from '@angular/fire/auth';
+import { AngularFirestoreModule } from '@angular/fire/firestore';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { GithubSearchComponent } from './github-search/github-search.component';
 import { SearchHistoryComponent } from './search-history/search-history.component';
 import { AccordionControlDirective } from './search-history/accordion-control.directive';
+import { environment } from 'src/environments/environment';
 
 @NgModule({
   declarations: [
@@ -17,6 +21,9 @@ import { AccordionControlDirective } from './search-history/accordion-control.di
     AccordionControlDirective,
   ],
   imports: [
+    AngularFireModule.initializeApp(environment.firebaseConfig),
+    AngularFireAuthModule,
+    AngularFirestoreModule,
     BrowserModule,
     AppRoutingModule,
     FormsModule,
