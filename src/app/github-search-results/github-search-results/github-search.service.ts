2be2da35eb7ab19f88e21db75df79c8afa6fa5b5
KylerJohnson26 <kylerljohnson@icloud.com>
2019-09-05 18:58:46 -0400

    adding firebase config, updating naming, and mapping to the github users property on the response in the service

--- src/app/github-search-results/github-search-results/github-search.service.ts
+++ src/app/github-search-results/github-search-results/github-search.service.ts
@@ -3,6 +3,9 @@ import { HttpClient, HttpParams } from '@angular/common/http';
 import { Observable } from 'rxjs';
 import { environment } from '../../../environments/environment';
 import { GithubUserSearchResponse } from './github-user-search-response';
+import { GithubUser } from './github-user';
+import { map } from 'rxjs/operators';
+import { GithubUserDetail } from './github-user-detail';
 
 @Injectable({
   providedIn: 'root'
@@ -14,9 +17,15 @@ export class GithubSearchService {
     private http: HttpClient
   ) { }
 
-  getGithubUsers(query: string) {
+  getGithubUsers(query: string): Observable<GithubUser[]> {
     const url = `${this.searchUsersUrl}?q=${query}`;
-    return this.http.get<GithubUserSearchResponse>(url);
+    return this.http.get<GithubUserSearchResponse>(url).pipe(
+      map(searchReponse => searchReponse.items)
+    );
+  }
+
+  getGithubUserDetail(user: GithubUser) {
+    return this.http.get<GithubUserDetail>(user.url);
   }
 
 }
--- src/app/github-search-results/github-search-results/results-list/github-user-card/github-user-card.component.ts
+++ src/app/github-search-results/github-search-results/results-list/github-user-card/github-user-card.component.ts
@@ -1,14 +1,36 @@
 import { Component, OnInit, Input } from '@angular/core';
 import { GithubUser } from '../../github-user';
+import { Observable } from 'rxjs';
+import { GithubUserDetail } from '../../github-user-detail';
+import { HttpClient } from '@angular/common/http';
+import { tap } from 'rxjs/operators';
 
 @Component({
   selector: 'app-github-user-card',
   templateUrl: './github-user-card.component.html',
   styleUrls: ['./github-user-card.component.scss']
 })
-export class GithubUserCardComponent {
+export class GithubUserCardComponent implements OnInit{
 
   @Input()
   githubUser: GithubUser;
+
+  githubUserDetail$: Observable<GithubUserDetail>;
+
+  constructor(
+    private http: HttpClient
+  ) {}
+
+  ngOnInit() {
+    console.log(this.githubUser)
+    this.githubUserDetail$ = this
+      .getGithubUserDetail(this.githubUser).pipe(
+        tap(detail => console.log(detail))
+      );
+  }
+
+  getGithubUserDetail(user: GithubUser) {
+    return this.http.get<GithubUserDetail>(user.url);
+  }
 
 }
