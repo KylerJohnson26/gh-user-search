351c90d280ebd2322745203590f2084ada6bdf8e
KylerJohnson26 <kylerljohnson@icloud.com>
2019-09-04 21:31:38 -0400

    renamed result component to github-user-card

--- src/app/github-search-results/github-search-results.module.ts
+++ src/app/github-search-results/github-search-results.module.ts
@@ -4,14 +4,14 @@ import { CommonModule } from '@angular/common';
 import { GithubSearchResultsRoutingModule } from './github-search-results-routing.module';
 import { GithubSearchResultsComponent } from './github-search-results/github-search-results.component';
 import { ResultsListComponent } from './github-search-results/results-list/results-list.component';
-import { ResultComponent } from './github-search-results/results-list/result/result.component';
+import { GithubUserCardComponent } from './github-search-results/results-list/github-user-card/github-user-card.component';
 
 
 @NgModule({
   declarations: [
     GithubSearchResultsComponent,
     ResultsListComponent,
-    ResultComponent
+    GithubUserCardComponent
   ],
   imports: [
     CommonModule,
