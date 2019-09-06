593104b2a7dba1de7a07dfe36bd9027e9d9b3c26
KylerJohnson26 <kylerljohnson@icloud.com>
2019-09-06 02:42:04 -0400

    adding logic to get users access token for the github request

---
+++ src/app/core/services/auth.service.ts
@@ -0,0 +1,48 @@
+import { Injectable } from '@angular/core';
+import { AngularFireAuth } from '@angular/fire/auth';
+import { auth } from 'firebase';
+import { Router } from '@angular/router';
+import { of } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AuthService {
+
+  constructor(
+    public afAuth: AngularFireAuth,
+    private router: Router
+  ) { }
+
+  githubAuth() {
+    return this.authLogin(new auth.GithubAuthProvider());
+  }
+
+  authLogin(provider) {
+    return this.afAuth.auth.signInWithRedirect(provider)
+      .then(success => {
+        console.log('successfully logged in');
+      }).catch(error => {
+        console.log(error);
+      });
+  }
+
+  getAccessToken() {
+    let token = null;
+    this.afAuth.auth.getRedirectResult()
+      .then((result) => {
+        token = (result as any).credential.accessToken;
+      });
+    console.log(token);
+    return of(token);
+  }
+
+
+  signout() {
+    this.afAuth.auth.signOut().then(() => {
+      console.log('Sign-out successful');
+    }).catch((error) => {
+      console.log(error);
+    });
+  }
+}
--- src/app/github-search-results/github-user-search/github-search-results/github-search.service.ts
+++ src/app/github-search-results/github-user-search/github-search-results/github-search.service.ts
@@ -1,11 +1,12 @@
 import { Injectable } from '@angular/core';
-import { HttpClient, HttpParams } from '@angular/common/http';
+import { HttpClient, HttpParams, HttpHeaders } from '@angular/common/http';
 import { Observable } from 'rxjs';
 import { environment } from '../../../../environments/environment';
 import { GithubUserSearchResponse } from './github-user-search-response';
 import { GithubUser } from './github-user';
-import { map } from 'rxjs/operators';
+import { map, switchMap } from 'rxjs/operators';
 import { GithubUserDetail } from './github-user-detail';
+import { AuthService } from 'src/app/core/services/auth.service';
 
 @Injectable({
   providedIn: 'root'
@@ -14,13 +15,20 @@ export class GithubSearchService {
   private searchUsersUrl = `${environment.githubApiUrl}/search/users`;
 
   constructor(
-    private http: HttpClient
+    private http: HttpClient,
+    private authService: AuthService
   ) { }
 
   getGithubUsers(query: string): Observable<GithubUser[]> {
     const url = `${this.searchUsersUrl}?q=${query}`;
-    return this.http.get<GithubUserSearchResponse>(url).pipe(
-      map(searchReponse => searchReponse.items)
+    const headers = new HttpHeaders();
+    return this.authService.getAccessToken().pipe(
+      switchMap(token => {
+        headers.set('Authorization', token);
+        return this.http.get<GithubUserSearchResponse>(url).pipe(
+          map(searchReponse => searchReponse.items)
+        );
+      }),
     );
   }
 
